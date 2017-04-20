51cfa5c03d67b1067339b73a9ac273bedea775ea
martapiecko <Marta.Piecko@Live.umcs.edu.pl>
2017-04-21 00:13:25 +0200

    First Commit

--- app/src/main/java/com/example/x/lekcja5_zadanie/MainActivity.java
+++ app/src/main/java/com/example/x/lekcja5_zadanie/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.x.lekcja5_zadanie;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.media.MediaPlayer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,14 +24,30 @@ public class MainActivity extends AppCompatActivity {
     private String[] tele;
     private Class[] activities={Tele1Activity.class, Tele2Activity.class, Tele3Activity.class};
 
+    static final private int ALERT_DIALOG_PLAIN = 1;
+    static final private int ALERT_DIALOG_BUTTONS = 2;
+    static final private int ALERT_DIALOG_LIST = 3;
+    static final private int CUSTOM_ALERT_DIALOG = 4;
+    private Button btnNewAlertDialog;
+    private Button btnNewAlertDialogButton;
+    private Button btnNewAlertDialogList;
+    private Button btnNewCustomAlertDialog;
+
+    private MediaPlayer mediaPlayer;
+    int music = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.telephones);
-
+        btnNewAlertDialog = (Button) findViewById(R.id.btnNewAlertDialog);
+        btnNewAlertDialogButton = (Button) findViewById(R.id.btnNewAlertDialogButton);
+        btnNewAlertDialogList = (Button) findViewById(R.id.btnNewAlertDialogList);
+        btnNewCustomAlertDialog = (Button) findViewById(R.id.btnNewCustomAlertDialog);
         initResources();
         initTelephonesListView();
+        initButtonsClick();
     }
 
     private void initResources(){
@@ -42,6 +65,133 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initButtonsClick() {
+        View.OnClickListener listener = new View.OnClickListener() {
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btnNewAlertDialog:
+                        showDialog(ALERT_DIALOG_PLAIN);
+                        break;
+                    case R.id.btnNewAlertDialogButton:
+                        showDialog(ALERT_DIALOG_BUTTONS);
+                        break;
+                    case R.id.btnNewAlertDialogList:
+                        showDialog(ALERT_DIALOG_LIST);
+                        break;
+                    case R.id.btnNewCustomAlertDialog:
+                        showDialog(CUSTOM_ALERT_DIALOG);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+        btnNewAlertDialog.setOnClickListener(listener);
+        btnNewAlertDialogButton.setOnClickListener(listener);
+        btnNewAlertDialogList.setOnClickListener(listener);
+        btnNewCustomAlertDialog.setOnClickListener(listener);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case ALERT_DIALOG_PLAIN:
+                return createPlainAlertDialog();
+            case ALERT_DIALOG_BUTTONS:
+                return createAlertDialogWithButtons();
+            case ALERT_DIALOG_LIST:
+                return createAlertDialogWithList();
+            case CUSTOM_ALERT_DIALOG:
+                return createCustomAlertDialog();
+            default:
+                return null;
+        }
+    }
+
+    private Dialog createPlainAlertDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Prosty dialog");
+        dialogBuilder.setMessage("Wiadomość prostego dialogu");
+        return dialogBuilder.create();
+    }
+
+    private Dialog createAlertDialogWithButtons() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Wyjście");
+        dialogBuilder.setMessage("Czy napewno?");
+        dialogBuilder.setCancelable(false);
+        dialogBuilder.setPositiveButton("Tak", new Dialog.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton) {
+                showToast("Wychodzę");
+                MainActivity.this.finish();
+            }
+        });
+        dialogBuilder.setNegativeButton("Nie", new Dialog.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton) {
+                showToast("Anulowaleś wyjście");
+            }
+        });
+        return dialogBuilder.create();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    private Dialog createAlertDialogWithList() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        final String[] options = {"Pierwsza piosenka", "Druga piosenka", "Trzecia piosenka"};
+        dialogBuilder.setTitle("Lista opcji");
+        dialogBuilder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int position) {
+                showToast("Wybrałeś: " + options[position]);
+                switch (position){
+                    case 0:
+                        music = R.raw.aidu;
+                        break;
+                    case 1:
+                        music = R.raw.aidu1;
+                        break;
+                    case 2:
+                        music = R.raw.aidu2;
+                }
+            }
+        });
+        return dialogBuilder.create();
+    }
+
+    private Dialog createCustomAlertDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        View layout = getCustomDialogLayout();
+        dialogBuilder.setView(layout);
+        dialogBuilder.setTitle("Custom Dialog");
+        return dialogBuilder.create();
+    }
+
+    private View getCustomDialogLayout() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        return inflater.inflate(R.layout.wlasny_layout, (ViewGroup)findViewById(R.id.wlasny));
+    }
+
+    public void playSound(View view){
+        if(mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+
+        if(music != -1) {
+            mediaPlayer = MediaPlayer.create(this, music);
+            mediaPlayer.start();
+        }
+    }
+
+    public void stopSound(View view){
+        if(mediaPlayer != null)
+            mediaPlayer.stop();
     }
 
     private Boolean exit = false;
